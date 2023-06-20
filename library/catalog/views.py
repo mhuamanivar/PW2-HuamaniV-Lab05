7f5503da2f918bed5b933aea2b9b160c7b1a1ec9
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2023-06-20 16:25:37 -0500

    Library creando página principal (5)

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+
+    # Extrae los registros usando objects.all()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Libros disponibles (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Crea y retorna una página HTML
+    return render(request, 'index.html', context=context)
